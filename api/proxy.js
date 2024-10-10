9d1c0a70611054de555093f75765fb567c8c7961
Ekushi <108033300+ExyXyz@users.noreply.github.com>
2024-10-10 19:29:57 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,11 +2,16 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   try {
+    // Check if the request is to the root domain
+    if (req.url === '/') {
+      return res.redirect('https://ekushi.xyz');
+    }
+
     // Forward the request to your VPS
     const response = await axios({
       method: req.method,
       url: `http://206.189.46.189${req.url}`,
-      timeout: 99000 // 60 seconds timeout
+      timeout: 99000 // 99 seconds timeout
     });
 
     // Send the VPS response back to the client
